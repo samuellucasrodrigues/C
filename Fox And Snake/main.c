a877c11cbdc33f6fc50774b0e3d3b10d9c14f577
Samuel Lucas <168255092+samuellucasrodrigues@users.noreply.github.com>
2025-03-23 13:03:46 -0300

    Fox And Snake

---
+++ Fox And Snake/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            
+            if (i == 0 || i % 2 == 0) {
+                printf("#");
+            }
+
+            else if (i % 4 == 1) {
+                if (j == m - 1) {
+                    printf("#");
+                } else {
+                    printf(".");
+                }
+            }
+            else if (i % 4 == 3) {
+                if (j == 0) {
+                    printf("#");
+                } else {
+                    printf(".");
+                }
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
